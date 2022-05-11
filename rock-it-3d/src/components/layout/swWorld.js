f77b15c3f161cd1af40b4ec487dde67ec3367f99
Di Xu <50468183+Di-Xu-FLIR@users.noreply.github.com>
2022-05-11 11:24:40 -0700

    load stl
    load stl

--- rock-it-3d/src/components/layout/swWorld.js
+++ rock-it-3d/src/components/layout/swWorld.js
@@ -1,13 +1,73 @@
-import React from "react";
-import Box from '../geometries/box';
+// import React from "react";
+// import Box from '../geometries/box';
+// import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
 
-const SWWorld = () => {
+// const SWWorld = () => {
+//     return (
+//         <>
+//             <Box position={[-2,2,4]} color={"pink"}/>
+//             <Box position={[-4,2,4]} color={"pink"}/>
+//             <Box position={[-2,4,4]} color={"cyan"}/>
+//             <Box position={[-4,4,4]} color={"cyan"}/>
+//         </>
+//     );
+// }
+
+// export default SWWorld;
+import React from 'react';
+
+import { useEffect, useRef } from "react";
+
+import { useThree } from '@react-three/fiber'
+
+import {STLLoader} from "three/examples/jsm/loaders/STLLoader";
+
+import {Canvas, useLoader} from "react-three-fiber";
+
+//import book from './book.stl'
+
+
+
+const NEWorld = () => {
+
+
+
+    const geom = useLoader(STLLoader, "./White_Brick_2_3.stl");
+
+
+
+    const ref = useRef();
+
+    const {camera} = useThree();
+
+    useEffect(() => {
+
+        camera.lookAt(ref.current.position);
+
+    });
+
+
+
     return (
-        <>
-            <Box position={[-2,2,4]} color={"pink"}/>
-            <Box position={[-4,2,4]} color={"pink"}/>
-        </>
-    );
-}
 
-export default SWWorld;
+        <>
+
+            <mesh ref={ref} position={[0,0,0]} scale={[0.1, 0.1, 0.1]}>
+
+                <primitive object={geom} attach="geometry"/>
+
+                <meshStandardMaterial color={"orange"}/>
+
+            </mesh>
+
+            <ambientLight/>
+
+            <pointLight position={[10, 10, 10]}/>
+
+        </>
+
+    );
+
+};
+
+export default NEWorld;
